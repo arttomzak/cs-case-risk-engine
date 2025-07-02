57bb9a1a0855643eb7603d7a64a35b8681ef8455
zaku <arttomzak@gmail.com>
2025-07-02 17:09:15 -0500

    initial repo setup

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Case Risk Engine is running!" << endl;
+    cout << "Press Enter to exit!" << endl;
+    cin.get();   
+    return 0;
+}
