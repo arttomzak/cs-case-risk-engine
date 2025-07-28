35ef90a47a63357cd69bfd3812dd79be5bcc4b48
zaku <arttomzak@gmail.com>
2025-07-27 23:56:42 -0500

    python GET reqest cleanup

--- include/drop.hpp
+++ include/drop.hpp
@@ -7,11 +7,21 @@ using namespace std;
 
 // this needs data on 
 
-// minimum + maximum float
-// names 
+// minimum + maximum float -> gonna need to parse diff json
+
+// markethashname -> StatTrak xxx (Fac New)
+// generate proper float then stattrak or nah then fetch price
+
+// quality (stat trak - normal or ST)
+
+
 
 struct Drop {
 
     string hash_name;
+    string quality;
+    double pricelatest;
+
+
 
 };
---
+++ scripts/fetch.py
@@ -0,0 +1,34 @@
+import requests
+import os 
+import json
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+API_KEY = os.getenv("STEAMWEBAPI_KEY")
+
+base_url = "https://www.steamwebapi.com/steam/api/cs/collection"
+
+# hardcoded for now, gonna want to pull from user input down the line
+slug = "the-revolver-case-collection"
+
+
+final_url = f"{base_url}/{slug}"
+
+
+response = requests.get(final_url, params={"key": API_KEY})
+
+if response.ok:
+    data = response.json()
+
+    filepath = f"data/{slug}.json"
+
+    with open(filepath, "w") as file:
+        json.dump(data, file, indent=2)
+
+    print(f"Data saved to {filepath}")
+
+else:
+    print(f"Request failed! {response.status_code}")
